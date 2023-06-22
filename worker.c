75381a4422610932d8afc066d2b399d38feab54c
Esteban Glochon <esteban.glochon@usach.cl>
2023-06-21 22:51:09 -0400

    optimizar worker

--- worker.c
+++ worker.c
@@ -1,14 +1,10 @@
-#include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <regex.h>
 #include <string.h>
 #include "fworker.h"
 
 int main(int argc, char *argv[])
 {
-	bool flag_loop = true;
 	char line_buffer[4096];
 	// FILE* pipe_fp = fdopen(stdin, "r");
 	regex_t re;
@@ -20,25 +16,18 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	while(flag_loop){
-		while(fgets(line_buffer, sizeof(line_buffer), stdin) != 0){
-			strtrim(line_buffer);
-			if(strcmp(line_buffer, "FIN")){
-				flag_loop = false;
-				break;
-			}
-			match_status = regexec(&re, line_buffer, (size_t)0, NULL, 0);
-			if (match_status == 0) {
-				printf("%s si\n", line_buffer);
-			}
-			else{
-				printf("%s no\n", line_buffer);
-			}
+	while(fgets(line_buffer, sizeof(line_buffer), stdin) != 0){
+		strtrim(line_buffer);
+		if(strcmp(line_buffer, "FIN") == 0) break;
+		match_status = regexec(&re, line_buffer, (size_t)0, NULL, 0);
+		if (match_status == 0) {
+			fprintf(stdout, "%s si\n", line_buffer);
 		}
-		sleep(1);
+		else{
+			fprintf(stdout, "%s no\n", line_buffer);
+		}
 	}
 
 	regfree(&re);
-
 	return 0;
 }
