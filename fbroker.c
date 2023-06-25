dd698504bacda2d28e5aa741808dc496518a55fe
Esteban Glochon <esteban.glochon@usach.cl>
2023-06-25 17:45:15 -0400

    fix include

--- fbroker.c
+++ fbroker.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
 
 // Descripción: Estructura que contiene datos relevantes para trabajar con un "worker".
 typedef struct {
