e872d06709d02625c3a7edf8a2ed52ef337394bf
Esteban Glochon <esteban.glochon@usach.cl>
2023-06-25 17:42:22 -0400

    libreria para worker y documentacion

--- fworker.c
+++ fworker.c
@@ -1,9 +1,15 @@
 #include <string.h>
 
+// Descripción: Comprueba si un caracter dado es de tipo "espacio".
+// Entradas:    Un character.
+// Salidas:     Retorna verdadero (1) o falso (0) dependiendo del caso.
 int is_space(char c){
 	return((c >= 0 && c <=32) || c == 127);
 };
 
+// Entradas: Recibe "string" (arreglo de carácteres).
+// Salidas: Retorna el mismo puntero del arreglo de caracteres.
+// Descripción: Remueve todos los carácteres "blancos" del inicio y final del string.
 char *strtrim(char *s) {
 	int start_index = 0;
 	int end_index = strlen(s);
